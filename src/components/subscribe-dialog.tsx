d34fba2e0d7c1c38daac5a86e5a90ca8d116ba84
hanzlah101 <116737045+hanzlah101@users.noreply.github.com>
2023-08-25 16:00:52 +0500

    Update subscribe-dialog.tsx

--- src/components/subscribe-dialog.tsx
+++ src/components/subscribe-dialog.tsx
@@ -4,6 +4,7 @@ import * as React from 'react'
 import { toast } from 'sonner'
 
 import { postData } from '@/libs/helpers'
+import { formatPrice } from '@/libs/utils'
 import { useUser } from '@/hooks/use-user'
 import { Button } from '@/components/ui/button'
 import { getStripe } from '@/libs/stripe-client'
@@ -25,50 +26,43 @@ interface SubscribeDialogProps {
 export const SubscribeDialog: React.FC<SubscribeDialogProps> = ({
   products,
 }) => {
-  const { isOpen, onClose } = useSubscribeDialog()
+  const subscribeDialog = useSubscribeDialog()
   const { user, isLoading, subscription } = useUser()
 
   const [priceIdLoading, setPriceIdLoading] = React.useState<string>()
 
-  const formatPrice = React.useCallback((price: Price) => {
-    const priceString = new Intl.NumberFormat('en-US', {
-      style: 'currency',
-      currency: price.currency,
-      minimumFractionDigits: 0,
-    }).format((price?.unit_amount || 0) / 100)
+  const onChange = (open: boolean) => {
+    if (!open) {
+      subscribeDialog.onClose()
+    }
+  }
 
-    return priceString
-  }, [])
+  const handleCheckout = async (price: Price) => {
+    setPriceIdLoading(price.id)
+    if (!user) {
+      setPriceIdLoading(undefined)
+      return toast.error('Must be logged in')
+    }
 
-  const handleCheckout = React.useCallback(
-    async (price: Price) => {
-      setPriceIdLoading(price.id)
-      if (!user) {
-        setPriceIdLoading(undefined)
-        return toast.error('Must be logged in')
-      }
+    if (subscription) {
+      setPriceIdLoading(undefined)
+      return toast('Already subscribed')
+    }
 
-      if (subscription) {
-        setPriceIdLoading(undefined)
-        return toast('Already subscribed')
-      }
+    try {
+      const { sessionId } = await postData({
+        url: '/api/create-checkout-session',
+        data: { price },
+      })
 
-      try {
-        const { sessionId } = await postData({
-          url: '/api/create-checkout-session',
-          data: { price },
-        })
-
-        const stripe = await getStripe()
-        stripe?.redirectToCheckout({ sessionId })
-      } catch (error) {
-        return toast.error((error as Error)?.message)
-      } finally {
-        setPriceIdLoading(undefined)
-      }
-    },
-    [user, subscription]
-  )
+      const stripe = await getStripe()
+      stripe?.redirectToCheckout({ sessionId })
+    } catch (error) {
+      return toast.error((error as Error)?.message)
+    } finally {
+      setPriceIdLoading(undefined)
+    }
+  }
 
   let content = <div className='text-center'>No products available.</div>
 
@@ -76,21 +70,20 @@ export const SubscribeDialog: React.FC<SubscribeDialogProps> = ({
     content = (
       <div>
         {products.map((product) => {
-          if (!product?.prices?.length) {
-            return
-          } else {
-            return product.prices.map((price) => (
-              <Button
-                size='lg'
-                key={price.id}
-                className='mb-4'
-                onClick={() => handleCheckout(price)}
-                disabled={isLoading || price.id === priceIdLoading}
-              >
-                {`Subscribe for ${formatPrice(price)} a ${price.interval}`}
-              </Button>
-            ))
+          if (!product.prices?.length) {
+            return <div key={product.id}>No prices available</div>
           }
+
+          return product.prices.map((price) => (
+            <Button
+              key={price.id}
+              className='mb-4'
+              onClick={() => handleCheckout(price)}
+              disabled={isLoading || price.id === priceIdLoading}
+            >
+              {`Subscribe for ${formatPrice(price)} a ${price.interval}`}
+            </Button>
+          ))
         })}
       </div>
     )
@@ -101,7 +94,10 @@ export const SubscribeDialog: React.FC<SubscribeDialogProps> = ({
   }
 
   return (
-    <Dialog open={isOpen} onOpenChange={onClose}>
+    <Dialog
+      open={subscribeDialog.isOpen}
+      onOpenChange={subscribeDialog.onClose}
+    >
       <DialogContent>
         <DialogHeader>
           <DialogTitle>Only for premium users</DialogTitle>
@@ -109,7 +105,7 @@ export const SubscribeDialog: React.FC<SubscribeDialogProps> = ({
             Listen to music with Spotify Premium
           </DialogDescription>
         </DialogHeader>
-        {content}
+        <div className='py-3'>{content}</div>
       </DialogContent>
     </Dialog>
   )
