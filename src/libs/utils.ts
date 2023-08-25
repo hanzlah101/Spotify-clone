6732238914c46bf0e76d7ba5fb3dc0ec2dad88e1
hanzlah101 <116737045+hanzlah101@users.noreply.github.com>
2023-08-25 16:01:17 +0500

    Update utils.ts

--- src/libs/utils.ts
+++ src/libs/utils.ts
@@ -1,8 +1,18 @@
 import { type ClassValue, clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
-import type { FileType } from '@/types'
+import { Price } from '@/types'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export const formatPrice = (price: Price) => {
+  const priceString = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: price.currency,
+    minimumFractionDigits: 0,
+  }).format((price?.unit_amount || 0) / 100)
+
+  return priceString
 }
